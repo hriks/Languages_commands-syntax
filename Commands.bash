425931cbbddf408b0050549fff32506fac50c060
Amit Kumar Gupta <hriks@users.noreply.github.com>
2016-10-25 17:17:40 +0530

    Create Commands.bash

---
+++ Commands.bash
@@ -0,0 +1,17 @@
+pwd         : Shows the cureent Directory
+cd          : Lets you in
+cd..        : get down one Directory
+ctrl+D      : Exit
+ctrl+C      : Terminate
+fg          : Foreground Process
+bg          : Background Process
+free        : Display Free Space
+git clone   : Lets you to Clone Git
+git Push    : Lets to Push you into Git
+touch       : Helps to Make File
+mkdir       : Make a new Folder
+cp          : Helps to Copy a file
+cp -r       : helps to Copy a floder
+rv          : Removes a file
+rv -r       : Removes a directory
+man         : get into details of the codes
