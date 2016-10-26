2322f024a8c4f3911e2af611bf466ff9bb025b28
Amit Kumar Gupta <hriks@users.noreply.github.com>
2016-10-26 14:44:07 +0530

    Update Commands.bash

--- Commands.bash
+++ Commands.bash
@@ -1,18 +1,29 @@
-pwd         : Shows the cureent Directory
-cd          : Lets you in
-cd..        : get down one Directory
-ctrl+D      : Exit 
-ctrl+C      : Terminate
-fg          : Foreground Process
-bg          : Background Process
-free        : Display Free Space
-git clone   : Lets you to Clone Git
-git Push    : Lets to Push you into Git
-touch       : Helps to Make File
-mkdir       : Make a new Folder
-cp          : Helps to Copy a file
-cp -r       : helps to Copy a floder
-rv          : Removes a file
-rv -r       : Removes a directory
-man         : get into details of the codes
+pwd                     : Shows the cureent Directory
+cd                      : Lets you in
+cd..                    : get down one Directory
+ctrl+D                  : Exit 
+ctrl+C                  : Terminate
+fg                      : Foreground Process
+bg                      : Background Process
+free                    : Display Free Space
+git clone               : Lets you to Clone Git
+git Push                : Lets to Push you into Git
+touch                   : Helps to Make File
+mkdir                   : Make a new Folder
+cp                      : Helps to Copy a file
+cp -r                   : helps to Copy a floder
+rv                      : Removes a file
+rv -r                   : Removes a directory
+man                     : get into details of the codes
+mv asas asas            : rename the file
+cat                     : places standard input itno the file
+more                    : output of the file content
+tail                    : Last 10 lines of the file
+head                    : First 10 lines of the file
+ps                      : active process
+grep                    : for searching
+date                    : display date
+time                    : Sisplay up time
+Cal                     : Display Calender
+tar cf file.tar files   : Creates a tar named file.tar
 
